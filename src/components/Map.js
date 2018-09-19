0c8625c613ebf4d807d817d4cca384f2fdd39278
wonjiky <wonjikyang@gmail.com>
2018-09-19 23:30:09 +0200

    Added mouse interaction for Agglos/WIP w/ click ix

--- src/components/Map.js
+++ src/components/Map.js
@@ -15,8 +15,7 @@ config.params = {
 	zoomSnap: 1.2,
 	minZoom: 3,
 	maxBounds:mybounds,
-	hasAlreadyUpdatedOnce: false,
-	opacity:0
+	opacity:0,
 };
 config.tileLayer = {
 	uri: 'https://api.mapbox.com/styles/v1/mkmd/cjj041lbo07vo2rphltlukpya/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWttZCIsImEiOiJjajBqYjJpY2owMDE0Mndsbml0d2V1ZXczIn0.el8wQmA-TSJp2ggX8fJ1rA',
@@ -32,17 +31,17 @@ class LeafletMap extends Component {
 		this.state = {
 			map: null,
 			tileLayer: null,
-			hi: 0,
 			list: 0,
-			hasAlreadyUpdatedOnce: false,
+			currLayerClicked: false,
 		};
 
-		this.onEachFeature = this.onEachFeature.bind(this);
+		// this.onEachFeature = this.onEachFeature.bind(this);
 		this.agglos_onEachFeature = this.agglos_onEachFeature.bind(this);
 		this.agglos_pointToLayer = this.agglos_pointToLayer.bind(this);
 		this.agglos_cityFilter = this.agglos_cityFilter.bind(this);
 		this.placeHolder_filter = this.placeHolder_filter.bind(this);
-		this.onChangeCountry = this.onChangeCountry.bind(this);
+		this.hoverStyle = this.hoverStyle.bind(this);
+		this.selectedStyle = this.selectedStyle.bind(this);
 	}
 
 	componentDidMount() {
@@ -50,7 +49,6 @@ class LeafletMap extends Component {
 		const tileLayer = L.tileLayer(config.tileLayer.uri, config.tileLayer.params).addTo(map);
 		this.setState({ map, tileLayer });
 
-		//Shading outisde border
 		this.mapShades = L.geoJson(this.props.africaOne, {
 			invert:true,
 			color:"grey",
@@ -59,117 +57,138 @@ class LeafletMap extends Component {
 		})
 		this.mapShades.addTo(map);
 
-		//PlaceHolder Layer
 		this.placeHolder = L.geoJSON(this.props.top50, {
 			filter: this.placeHolder_filter,
-			pointToLayer: function (feature, latlng) {
-			return L.circleMarker(latlng);}
+			// pointToLayer: function (feature, latlng) {
+			// return L.circleMarker(latlng);}
 			})
 		this.placeHolder.addTo(map);
 	}
 
-	componentDidUpdate(prevProps){
-		this.mapOverlay = L.geoJson(this.props.africaContinent, {
-			onEachFeature:this.onEachFeature,
+	selectedStyle(){
+		return({
+			weight : 2,
+			color : 'black',
+			fillColor : 'yellow',
+			fillOpacity : 0.03,
 		})
-		this.mapOverlay.addTo(this.state.map);
-		this.mapOverlay.setStyle({
-			fillOpacity: 0,
-			color: 'transparent'
-		})
+	}
 
+	hoverStyle(){
+		return({
+			color: '#E8AE40',
+			weight: 3,
+			stroke:true,
+			fillOpacity: 0,
+		})
+	}
+
+	componentDidUpdate(prevProps){
 		let currValue = this.props.selectedCountry;
 		let prevValue = prevProps.selectedCountry;
+		if(currValue){
+			this.currLayer = this.mapOverlay.getLayer(currValue);
+			this.prevLayer = this.mapOverlay.getLayer(prevValue);
+		}
+
+		this.mapOverlay = L.geoJson(this.props.africaContinent, {
+			style: () => {return {color: 'transparent'}},
+			onEachFeature: (feature, layer) => {
+				
+				layer.on('mouseover', () => {
+					layer.setStyle(this.hoverStyle());
+					//@@ INHOI @@//
+					// if(feature.properties.hasFocus === true){
+					// 	layer.setStyle(this.selectedStyle());
+					// }
+				});
+		
+				layer.on('mouseout', (e) => {
+					this.mapOverlay.resetStyle(e.target);
+					
+					//@@ INHOI @@//
+					// if(feature.properties.hasFocus === true){
+					// 	layer.setStyle(this.selectedStyle());
+					// }
+				});
+
+				layer.on('change', (e) => {
+					this.placeHolder.clearLayers();
+					this.state.map.fitBounds(layer.getBounds());
+					this.ISO3_CODE = feature.properties.ISO3_CODE;
+					this.agglos = L.geoJson(this.props.agglosGeo, {
+						onEachFeature: this.agglos_onEachFeature,
+						filter: this.agglos_cityFilter,
+						pointToLayer: this.agglos_pointToLayer
+					});
+					
+
+					//@@ INHOI @@//
+					// feature.properties.hasFocus = true;
+					// this.currLayer.setStyle(this.selectedStyle());
+
+					this.placeHolder.addLayer(this.agglos);
+				});
+
+				layer.on('click', () => {
+					const ISO3_ID = feature.properties.ID;
+					const ISO3_NAME = feature.properties.NAME_EN;
+					const e = { value: ISO3_ID, label:ISO3_NAME}
+					this.props.handleISO(e);
+				});
+		
+				layer._leaflet_id = feature.properties.ID;
+			}
+		})
+		this.mapOverlay.addTo(this.state.map);
+
 		if(currValue !== prevValue){
-			var layer = this.mapOverlay.getLayer(currValue);
-			layer.fireEvent('change', currValue)
+			let layer = this.mapOverlay.getLayer(currValue);
+			layer.fire('change')
 		}
 	}
 
+	// onEachFeature(feature, layer){
+	// 	layer.on('mouseover', () => {
+	// 		layer.setStyle({
+	// 		fillOpacity: 0.6,
+	// 		color: '#E8AE40',
+	// 		stroke: 2,
+	// 		weight: 2
+	// 		});
+	// 	});
+		
+	// 	layer.on('mouseout', () => {
+	// 		layer.setStyle({
+	// 		fillOpacity: 0.0,
+	// 		color: 'transparent'
+	// 		});
+	// 	});
 
+	// 	layer.on('click', () => {
+	// 		const ISO3_ID = feature.properties.ID;
+	// 		const ISO3_NAME = feature.properties.NAME_EN;
+	// 		const e = { value: ISO3_ID, label:ISO3_NAME}
+	// 		this.props.handleISO(e);
+	// 	});
 
-	onEachFeature(feature, layer){
-		layer.on('mouseover', () => {
-			layer.setStyle({
-			fillOpacity: 0.6,
-			color: '#E8AE40',
-			weight: 0
+	// 	layer.on('change', (e) => {
+	// 		this.placeHolder.clearLayers();
+			
+	// 		this.state.map.fitBounds(layer.getBounds());
+	// 		this.ISO3_CODE = feature.properties.ISO3_CODE;
 
-			});
-			if(feature.properties.clicked==true){layer.setStyle({
-				weight : 2,
-									color : 'black',
-									fillColor : 'yellow',
-									fillOpacity : 0.03,
+	// 		this.agglos = L.geoJson(this.props.agglosGeo, {
+	// 			onEachFeature: this.agglos_onEachFeature,
+	// 			filter: this.agglos_cityFilter,
+	// 			pointToLayer: this.agglos_pointToLayer
+	// 		});
 
-				});}
-		});
+	// 		this.placeHolder.addLayer(this.agglos);
+	// 	});
 
-		layer.on('mouseout', () => {
-			layer.setStyle({
-			fillOpacity: 0.0,
-			color: 'transparent'
-			});
-			if(feature.properties.clicked==true){layer.setStyle({
-				weight : 2,
-									color : 'black',
-									fillColor : 'yellow',
-									fillOpacity : 0.03,
-
-				});}
-		});
-
-		layer.on('click', () => {
-
-
-			const ISO3_ID = feature.properties.ID;
-			const ISO3_NAME = feature.properties.NAME_EN;
-			const e = { value: ISO3_ID, label:ISO3_NAME}
-			feature.properties.clicked=true;
-			this.props.handleISO(e);
-			layer.setStyle({
-				weight : 2,
-                    color : 'black',
-                    fillColor : 'yellow',
-                    fillOpacity : 0.03,
-
-			});
-		});
-
-		layer.on('change', (e) => {
-
-			this.placeHolder.clearLayers();
-
-			this.state.map.fitBounds(layer.getBounds());
-			this.ISO3_CODE = feature.properties.ISO3_CODE;
-
-			this.agglos = L.geoJson(this.props.agglosGeo, {
-				onEachFeature: this.agglos_onEachFeature,
-				filter: this.agglos_cityFilter,
-				pointToLayer: this.agglos_pointToLayer
-			});
-
-			this.placeHolder.addLayer(this.agglos);
-
-		});
-		layer._leaflet_id = feature.properties.ID;
-	}
-
-	onChangeCountry(feature, layer){
-		this.state.map.fitBounds(layer.getBounds());
-
-		this.agglos = L.geoJson(this.props.agglosGeo, {
-			onEachFeature: this.agglos_onEachFeature,
-			filter: this.agglos_cityFilter,
-			pointToLayer: this.agglos_pointToLayer
-		});
-
-
-		this.placeHolder.clearLayers();
-		const ISO3_CODE = feature.properties.ISO3_CODE;
-		this.setState({ISO3_CODE});
-		this.placeHolder.addLayer(this.agglos);
-	}
+	// 	layer._leaflet_id = feature.properties.ID;
+	// }
 
 	//Year filter for citieslist
 	placeHolder_filter(feature) {
@@ -189,16 +208,41 @@ class LeafletMap extends Component {
 	}
 
 	agglos_pointToLayer(feature, latlng){
-		const geojsonMarker = {
+		const geojsonMarker = this.defaultAgglosStyle();
+		return L.circleMarker(latlng, geojsonMarker);
+	}
+
+	highlightAgglosStyle(){
+		return({
+			radius: 10,
+			fillOpacity: 1,
+			fillColor: "red",
+			weight: 0,
+		})
+	}
+
+	defaultAgglosStyle(){
+		return({
 			radius: 4,
 			fillColor: "#FFF",
 			color: "#E8AE40",
 			weight: 1,
-		};
-		return L.circleMarker(latlng, geojsonMarker);
+		})
 	}
 
 	agglos_onEachFeature(feature, layer) {
+		layer.on('mouseover', (e) => {
+			e.target.setStyle(this.highlightAgglosStyle())
+		})
+
+		layer.on('mouseout', (e) => {
+			e.target.setStyle(this.defaultAgglosStyle())
+		})
+
+		layer.on('click', (e) => {
+			e.target.setStyle(this.highlightAgglosStyle())
+		})
+
 		let popupContent = "<table class='tooltip-table'>";
 		popupContent += "<tr><td class='title'>Name:</td><td class='data'>" + feature.properties.NAME + "</td></tr>";
 		popupContent += "<tr><td class='title'>Population:</td><td class='data'>" + feature.properties.PTA2015 + "</td></tr>";

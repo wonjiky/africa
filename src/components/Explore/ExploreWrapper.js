8aa3f699b6a2368ae8a417593a0af8e50e864202
Inhoi.HEO <inhoi.heo@oecd.org>
2018-11-15 12:08:06 +0100

    'bugfix'

--- src/components/Explore/ExploreWrapper.js
+++ src/components/Explore/ExploreWrapper.js
@@ -42,26 +42,26 @@ class ExploreWrapper extends Component {
 
     componentDidUpdate(preProps, prevState) {
         if(prevState.searchOption !== this.state.searchOption){
-            
-                this.setState({
-                    origin: '',
-                    selectedAgglos: '',
-                    selectedCountry: '',
-                    timeSliderValue: 2015,
-                    sizeArray: '',
-                    timeSliderRange: [
-                        { value: 1950, step: 10 },
-                        { value: 1960, step: 10 },
-                        { value: 1970, step: 10 },
-                        { value: 1980, step: 10 },
-                        { value: 1990, step: 10 },
-                        { value: 2000, step: 10 },
-                        { value: 2010, step: 5 },
-                        { value: 2015 }
-                    ],
-                    firstCompareValue: null,
-                    secondCompareValue: null,
-                })
+
+                // this.setState({
+                //     origin: '',
+                //     selectedAgglos: '',
+                //     selectedCountry: '',
+                //     timeSliderValue: 2015,
+                //     sizeArray: '',
+                //     timeSliderRange: [
+                //         { value: 1950, step: 10 },
+                //         { value: 1960, step: 10 },
+                //         { value: 1970, step: 10 },
+                //         { value: 1980, step: 10 },
+                //         { value: 1990, step: 10 },
+                //         { value: 2000, step: 10 },
+                //         { value: 2010, step: 5 },
+                //         { value: 2015 }
+                //     ],
+                //     firstCompareValue: null,
+                //     secondCompareValue: null,
+                // })
             }
         }
 
@@ -71,7 +71,7 @@ class ExploreWrapper extends Component {
                 this.setState({selectedAgglos: ''})
             }
         }
-        let selectedValue = a === null ? '' : a.value;        
+        let selectedValue = a === null ? '' : a.value;
         this.setState({ selectedCountry: selectedValue})
     }
 
